ee570fef466e835ddf7ba66ceb1ff2eb3f601900
ZhuZhuZhuang10 <157409639+ZhuZhuZhuang10@users.noreply.github.com>
2024-06-08 08:41:43 +0200

    Delete functions/src directory

--- functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Dependencies
-import * as functions from 'firebase-functions';
-
-// Function Import
-import { NestServer } from "./nest-api/nest-server";
-
-// Functions Implementation
-export const api = functions.runWith({ memory: '2GB', timeoutSeconds: 150 }).https.onRequest(NestServer);
